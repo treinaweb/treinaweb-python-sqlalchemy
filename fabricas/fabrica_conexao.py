507aa506471ec93a3fb3c9a18d08ed135dfaf289
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-10-25 14:32:09 -0300

    03 - Conectando ao BD

--- fabricas/fabrica_conexao.py
+++ fabricas/fabrica_conexao.py
@@ -1,15 +1,19 @@
 import MySQLdb, configparser
+from sqlalchemy import create_engine
+
 
 class FabricaConexao():
 
     def conectar(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        db = MySQLdb.connect(user=config['DATABASE']['user'],
-                             passwd=config['DATABASE']['passwd'],
-                             db=config['DATABASE']['db'],
-                             host=config['DATABASE']['host'],
-                             port=int(config['DATABASE']['port']),
-                             autocommit=config['DATABASE']['autocommit'])
 
-        return db
+        user = config['DATABASE']['user']
+        passwd = config['DATABASE']['passwd']
+        db = config['DATABASE']['db']
+        host = config['DATABASE']['host']
+        port = int(config['DATABASE']['port'])
+
+        engine = create_engine(f'mysql://{user}:{passwd}@{host}:{port}/{db}')
+
+        return engine

107603f8f5b6a738da1376123dd44030db2667f7
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-10-31 18:49:05 -0300

    13 - Criando e relacionando entidades 1N

--- dominios/db.py
+++ dominios/db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 from fabricas import fabrica_conexao
 
@@ -15,7 +16,19 @@ class Cliente(Base):
     nome = Column(String(40), nullable=False)
     idade = Column(Integer, nullable=False)
 
+    pedidos = relationship("Pedido", back_populates="cliente")
+
     def __repr__(self):
         return "Cliente %s ('%s', '%s')" % (self.id, self.nome, self.idade)
+
+class Pedido(Base):
+    __tablename__ = 'pedido'
+    id = Column(Integer, primary_key=True)
+
+    cliente_id = Column(Integer, ForeignKey('cliente.id'), nullable=False)
+    cliente = relationship("Cliente", back_populates="pedidos")
+
+    def __repr__(self):
+        return "Pedido %s " % (self.id)
 
 Base.metadata.create_all(engine)
